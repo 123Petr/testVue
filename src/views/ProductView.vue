d4878bd4be3db21ccd4577faf92fa756c10588f0
123Petr <kuppeter@gmail.com>
2024-07-15 23:21:47 +0300

    first commit

---
+++ src/views/ProductView.vue
@@ -0,0 +1,44 @@
+<template>
+        <section class='products'>
+                <div class="container">
+                    <div class="products-wrapper" id='goods-container'>
+                        <div 
+                            class="products-card" 
+                            v-for='good in goodsArray'
+                            :key='good.id'>
+                            <div class="products-card__image">
+                                <img :src='getUrl(good.image)' alt='good-1' />
+                            </div>
+                            <div class="products-card__description">
+                                <div class="product-card__description-row">
+                                    <h5 class="products-card__description--name">
+                                        {{good.title}}
+                                    </h5>
+                                </div>
+                                <div class="product-card__description-row">
+                                    <p class="products-card__description--text">
+                                        {{good.descr}}
+                                    </p>
+                                </div>
+                                <div class="product-card__description-row">
+                                    <div class="products-card__description-controls">
+                                        <button class='btn btn-primary'>
+                                            В корзину
+                                            <img src='../assets/images/icons/shopping-cart-white.svg' alt='shopping-cart'>
+                                        </button>
+                                        <span class='products-card__description-controls--price'>{{good.price}} ₽</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>             
+                    </div>
+                </div>
+            </section>
+</template>
+
+<script setup>
+import {goodsArray} from '../constants/goods'
+const getUrl=(name)=>{
+  return new URL(`../assets/images/goods/${name}`, import.meta.url);
+}    
+</script>
